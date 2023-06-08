ece97f30d0c6a944fbc40431ae6f9e1ea365437a
Guilherme <devguicordeiro@gmail.com>
2023-06-07 22:15:44 -0300

    navbar partialy complete

--- components/header.js
+++ components/header.js
@@ -1,23 +1,48 @@
 import Link from "next/link";
 import { styled } from "styled-components";
+import Center from "./center";
 
 const StyledHeader = styled.header `
     background-color: #333;
+`;
+const Logo = styled(Link)`
+    color: #fff;
+    text-decoration: none;
+`;
+const Wrapper = styled.div`
+display: flex;
+justify-content: space-between;
+padding: 10px 0;
+`;
+
+const StyledNav = styled.nav `
+    display: flex;
+    gap: 15px;
+`;
+
+const NavLink = styled(Link) `
+    color: #aaa;
+    text-decoration: none;
+
 `;
 
 export default function Header() {
     return (
         <StyledHeader>
-            <Link href={"/"}>
-                Fofos
-            </Link>
-            <nav>
-                <Link href={"/"}>Início</Link>
-                <Link href={"/products"}>Produtos</Link>
-                <Link href={"/categories"}>Categorias</Link>
-                <Link href={"/account"}>Conta</Link>
-                <Link href={"/cart"}>Carrinho (0)</Link>
-            </nav>
+            <Center>
+                <Wrapper>
+                    <Logo href={"/"}>
+                        Fofos
+                    </Logo>
+                    <StyledNav>
+                        <NavLink href={"/"}>Início</NavLink>
+                        <NavLink href={"/products"}>Produtos</NavLink>
+                        <NavLink href={"/categories"}>Categorias</NavLink>
+                        <NavLink href={"/account"}>Conta</NavLink>
+                        <NavLink href={"/cart"}>Carrinho (0)</NavLink>
+                    </StyledNav>
+                </Wrapper>
+            </Center>
         </StyledHeader>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
   body{
     padding: 0;
     margin: 0;
+    font-family: 'Ubuntu', sans-serif;
   }
 `;
 
