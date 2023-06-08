807e85e7192f97ef36bc98bdc7607d96919afe0b
Guilherme <devguicordeiro@gmail.com>
2023-06-08 14:01:50 -0300

    featured buttons

---
+++ components/Button.js
@@ -0,0 +1,32 @@
+import { css, styled } from "styled-components"
+
+const StyledButton = styled.button`
+    border: 0;
+    padding: 5px 15px;
+    border-radius: 5px;
+    cursor: pointer;
+    ${props => props.primary && css`
+        background-color: #581c87;
+        color: #fff;
+        border: 1px solid #581c87;
+    `}
+    ${props => props.white && !props.outline && css`
+        background-color: #fff;
+        color: #000;
+    `}
+    ${props => props.white && props.outline && css`
+        background-color: transparent;
+        color: #fff;
+        border: 1px solid #fff;
+`}
+    ${props => props.size === "l" && css`
+        font-size: 1.2rem;
+        padding: 10px 20px
+    `}
+`;
+
+export default function Button({children, ...rest}) {
+    return(
+        <StyledButton {...rest}>{children}</StyledButton>
+    )
+}
--- components/Featured.js
+++ components/Featured.js
@@ -1,5 +1,6 @@
 import { styled } from "styled-components";
 import Center from "./center";
+import Button from "./Button";
 
 const Bg = styled.div`
  background-color: #333;
@@ -14,7 +15,7 @@ const Desc = styled.p`
     color: #aaa;
     font-size: .8rem;s
 `;
-const Wrapper = styled.div `
+const ColumnsWrapper = styled.div `
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 40px;
@@ -26,22 +27,30 @@ const Column = styled.div`
     display: flex;
     align-items: center;
 `;
+const ButtonsWrapper = styled.div`
+    display: flex;
+    gap: 5px;
+`;
 
 export default function Featured() {
     return(
         <Bg>
             <Center>
-                <Wrapper>
+                <ColumnsWrapper>
                     <Column>
                     <div>
-                          <Title>Pro</Title>
+                        <Title>Pro</Title>
                         <Desc>lajsdoiasjdio jsaoi djasoijd asoijd saoi aksdopka asokdpoak oaksd poakdp okapdo kapodks  asodkopakdpoaksdpoakks asokd opakd opakpok asok</Desc>
+                        <ButtonsWrapper>
+                            <Button outline white size={"l"}>Saiba mais</Button>
+                            <Button primary size={"l"}>Adicionar ao carrinho</Button>
+                        </ButtonsWrapper>
                     </div>
                    </Column>
                     <Column>
                         <img src="https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg" alt="alt"/>
                     </Column>
-                </Wrapper>
+                </ColumnsWrapper>
             </Center>
         </Bg>
     )
