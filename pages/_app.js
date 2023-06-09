5b450d8c296e9101f133004efdf8f2f1c6fe2cc5
Guilherme <devguicordeiro@gmail.com>
2023-06-09 19:14:24 -0300

    all bug solveds. finally i can move on

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
-  body{
+  @font-face {
+    font-family: 'Ubuntu';
+    font-style: normal;
+    font-weight: 400;
+    src: url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+  }
+
+  body {
     padding: 0;
     margin: 0;
     font-family: 'Ubuntu', sans-serif;
@@ -12,7 +18,7 @@ const GlobalStyles = createGlobalStyle`
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyles/>
+      <GlobalStyles />
       <Component {...pageProps} />
     </>
   );
