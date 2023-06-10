30c921888a6cf4eb0cf4f6c29a6aaa4768ef16d1
Guilherme <devguicordeiro@gmail.com>
2023-06-10 16:11:07 -0300

    added cart btn and prices to latests boxes

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -1,4 +1,6 @@
 import { styled } from "styled-components"
+import Button from "./Button";
+import CartIcon from "./icons/CartIcon";
 
 const ProductWrapper = styled.div`
 
@@ -7,7 +9,7 @@ const ProductWrapper = styled.div`
 const FuchsiaBox = styled.div`
     background-color: fuchsia;
     padding: 20px;
-    height: 150px;
+    height: 120px;
     text-align:center;
     display: flex;
     align-items: center;
@@ -15,19 +17,46 @@ const FuchsiaBox = styled.div`
     border-radius: 5px;
     img{
         max-width: 100%;
-        max-height: 150px;
+        max-height: 80px;
     }
+`;
+
+const Title = styled.h2`
+    font-weight: normal;
+    font-size: 1rem;
+    margin: 0;
+`;
+
+const ProductInfoBox = styled.div `
+    margin-top: 2px;
+`;
+
+const PriceRow = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Price = styled.div`
+    font-size: 1.2rem;
+    font-weight: 500;
 `;
 
 export default function ProductBox({_id, title, description, price, images}) {
     return(
         <ProductWrapper>
-        <FuchsiaBox>
-            <div>
-               <img src={images[0]}></img>
-            </div>
-        </FuchsiaBox>
-        {title}
+            <FuchsiaBox>
+                <div>
+                <img src={images[0]}></img>
+                </div>
+            </FuchsiaBox>
+            <ProductInfoBox>
+                <Title>{title}</Title>
+                <PriceRow>
+                    <Price>R$ {price}</Price>
+                    <Button primary={1}><CartIcon/></Button>
+                </PriceRow>
+            </ProductInfoBox>
         </ProductWrapper>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export async function getServerSideProps() {
   const featuredProductId = "64752d48bc69b54eb8c26185";
   await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
-  const newProducts = await Product.find({}, null, {sort: {"_id":-1}, limit:10});
+  const newProducts = await Product.find({}, null, {sort: {"_id":-1}, limit:12});
   return {
     props: {featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
             newProducts: JSON.parse(JSON.stringify(newProducts)),
