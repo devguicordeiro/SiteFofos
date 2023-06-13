8503b637a13881436c4dec1aacc46e44e22d6ca6
Guilherme <devguicordeiro@gmail.com>
2023-06-13 09:51:47 -0300

    added add and remove products through cart

--- components/CartContext.js
+++ components/CartContext.js
@@ -18,8 +18,17 @@ export function CartContextProvider ({children} ) {
     function addProduct(productId) {
         setCartProducts(prev => [...prev, productId]);
     }
+    function removeProduct(productId) {
+        setCartProducts(prev => {
+            const pos = prev.indexOf(productId);
+            if(pos !== -1) {
+               return prev.filter((value, index) => index !== pos);
+            }
+            return prev;
+        });
+    }
     return (
-        <CartContext.Provider value={{cartProducts, setCartProducts, addProduct}}>
+        <CartContext.Provider value={{cartProducts, setCartProducts, addProduct, removeProduct}}>
             {children}
         </CartContext.Provider>
     )
--- pages/cart.js
+++ pages/cart.js
@@ -8,96 +8,110 @@ import { useContext, useEffect, useState } from "react";
 import { styled } from "styled-components";
 
 const ColumnsWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1.3fr .7fr;
-    gap: 40px;
-    margin-top: 40px;
+  display: grid;
+  grid-template-columns: 1.3fr .7fr;
+  gap: 40px;
+  margin-top: 40px;
 `;
 
 const Box = styled.div`
-    background-color: #fff;
-    border-radius: 5px;
-    padding: 30px;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 30px;
 `;
 
 const ProductInfoCell = styled.div `
-    padding: 10px 0;
-    border-top: 1px solid rgba(0, 0, 0, 0.2);
+  padding: 10px 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.2);
 `;
 
 const ProductImageBox = styled.div`
-    img{
-        max-width: 80px;
-        max-height: 80px;
+  img {
+    max-width: 80px;
+    max-height: 80px;
+  }
+  width: 100px;
+  height: 100px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-    };
-    width: 100px;
-    height: 100px;
-    padding: 10px;
-    border-radius: 5px;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
+const QuantityLabel = styled.span`
+  padding: 0 5px;
 `;
 
 export default function CartPage() {
-    const {cartProducts} = useContext(CartContext);
-    const [products, setProducts] = useState ([]);
-    useEffect (() => {
-        if (cartProducts.length > 0) {
-            axios.post("/api/cart", {ids:cartProducts}).then(response => {
-                setProducts(response.data);
-            })
-        }
-    }, [cartProducts]); 
-    return (
-        <>
-            <Header></Header>
-            <Center>
-            <ColumnsWrapper>
-                <Box>
-                <h2>Carrinho</h2>
-                    {!cartProducts?.length && (
-                        <div>Seu carrinho está vazio</div>
-                    )}
-                    {products?.length > 0 && (
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Produto</th>
-                                <th>Quantidade</th>
-                                <th>Preço</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {products.map(product => (
-                                    <tr>
-                                        <ProductInfoCell>
-                                            <ProductImageBox>
-                                                <img src={product.images[0]} alt=""/>
-                                            </ProductImageBox>
-                                                {product.title}
-                                            </ProductInfoCell>
-                                        <td>{cartProducts.filter(id => id === product._id).length}</td>
-                                        <td>Price</td>
-                                    </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                    )}
-                </Box>
-                {!!cartProducts?.length && (
-                                    <Box>
-                                        <h2>Informação do pedido</h2>
-                                        <input type="text"></input>
-                                        <input type="text"></input>
-                                        <Button block={1} black={1}>Continuar para pagamento</Button>
-                                     </Box>
-                )}
-            </ColumnsWrapper>
-            </Center>
-        </>
-    )
+  const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (cartProducts.length > 0) {
+      axios.post("/api/cart", { ids: cartProducts }).then(response => {
+        setProducts(response.data);
+      });
+    }
+  }, [cartProducts]);
+
+  function plusProduct(id) {
+    addProduct(id);
+  }
+
+  function lessProduct(id) {
+    removeProduct(id);
+  }
+
+  return (
+    <>
+      <Header></Header>
+      <Center>
+        <ColumnsWrapper>
+          <Box>
+            <h2>Carrinho</h2>
+            {!cartProducts?.length && <div>Seu carrinho está vazio</div>}
+            {products?.length > 0 && (
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Produto</th>
+                    <th>Quantidade</th>
+                    <th>Preço</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map(product => (
+                    <tr>
+                      <ProductInfoCell>
+                        <ProductImageBox>
+                          <img src={product.images[0]} alt="" />
+                        </ProductImageBox>
+                        {product.title}
+                      </ProductInfoCell>
+                      <td>
+                        <Button onClick={() => lessProduct(product._id)}>-</Button>
+                        <QuantityLabel>{cartProducts.filter(id => id === product._id).length}</QuantityLabel>
+                        <Button onClick={() => plusProduct(product._id)}>+</Button>
+                      </td>
+                      <td>R${cartProducts.filter(id => id === product._id).length * product.price}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )}
+          </Box>
+          {!!cartProducts?.length && (
+            <Box>
+              <h2>Informação do pedido</h2>
+              <input type="text"></input>
+              <input type="text"></input>
+              <Button block={1} black={1}>Continuar para pagamento</Button>
+            </Box>
+          )}
+        </ColumnsWrapper>
+      </Center>
+    </>
+  );
 }
