5462699001efa17fdea96c2287599709d566058c
Guilherme <devguicordeiro@gmail.com>
2023-06-15 16:47:58 -0300

    kinda of stuck in a bug with fast reload, coming to solve in a sec

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,58 +1,38 @@
-import React, { createContext, useEffect, useState } from "react";
+import {createContext, useEffect, useState} from "react";
 
 export const CartContext = createContext({});
 
-export function CartContextProvider({ children }) {
+export function CartContextProvider({children}) {
   const ls = typeof window !== "undefined" ? window.localStorage : null;
-  const [cartProducts, setCartProducts] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-
+  const [cartProducts,setCartProducts] = useState([]);
   useEffect(() => {
-    if (!isLoaded && ls && ls.getItem("cart")) {
-      setCartProducts(JSON.parse(ls.getItem("cart")));
-      setIsLoaded(true);
+    if (cartProducts?.length > 0) {
+      ls?.setItem('cart', JSON.stringify(cartProducts));
     }
-  }, [ls, isLoaded]);
-
+  }, [cartProducts]);
   useEffect(() => {
-    if (isLoaded) {
-      if (cartProducts?.length > 0) {
-        ls?.setItem("cart", JSON.stringify(cartProducts));
-      } else {
-        ls?.removeItem("cart");
-      }
+    if (ls && ls.getItem('cart')) {
+      setCartProducts(JSON.parse(ls.getItem('cart')));
     }
-  }, [cartProducts, ls, isLoaded]);
-
+  }, []);
   function addProduct(productId) {
-    setCartProducts((prev) => [...prev, productId]);
+    setCartProducts(prev => [...prev,productId]);
   }
-
   function removeProduct(productId) {
-    setCartProducts((prev) => {
+    setCartProducts(prev => {
       const pos = prev.indexOf(productId);
       if (pos !== -1) {
-        return prev.filter((value, index) => index !== pos);
+        return prev.filter((value,index) => index !== pos);
       }
       return prev;
     });
   }
-
   function clearCart() {
     setCartProducts([]);
   }
-
-  const cartContextValue = {
-    cartProducts,
-    addProduct,
-    removeProduct,
-    clearCart,
-  };
-
   return (
-    <CartContext.Provider value={cartContextValue}>
+    <CartContext.Provider value={{cartProducts,setCartProducts,addProduct,removeProduct,clearCart}}>
       {children}
     </CartContext.Provider>
   );
 }
-
--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,34 +1,42 @@
-import { mongooseConnect } from "@/lib/mongoose";
-const stripe = require("stripe")(process.env.STRIPE_SK);
-import { buffer } from "micro/types/src/lib";
+import {mongooseConnect} from "@/lib/mongoose";
+const stripe = require('stripe')(process.env.STRIPE_SK);
+import {buffer} from 'micro';
+import {Order} from "@/models/Order";
 
 const endpointSecret = "whsec_5a115d5dad247676fa555bcfbf26a567b06c61d8658033e771607a4ed22d4055";
 
-export default async function handler (req, res) {
-    await mongooseConnect();
-    const sig = req.headers['stripe-signature'];
+export default async function handler(req,res) {
+  await mongooseConnect();
+  const sig = req.headers['stripe-signature'];
 
-    let event;
-  
-    try {
-      event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
-    } catch (err) {
-      res.response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
-  
-    // Handle the event
-    switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntentSucceeded = event.data.object;
-        // Then define and call a function to handle the event payment_intent.succeeded
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === 'paid';
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId,{
+          paid:true,
+        })
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send('ok');
 }
 
 export const config = {
-    api: {bodyParser:false,}
-}
+  api: {bodyParser:false,}
+};
