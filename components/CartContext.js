d61dcf1688a0df8e8fe98228a75027d3c0645068
Guilherme <devguicordeiro@gmail.com>
2023-06-15 13:39:13 -0300

    added clear cart after payment

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,35 +1,57 @@
-import { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext({});
 
-export function CartContextProvider ({children} ) {
-    const ls = typeof window !== "undefined" ? window.localStorage : null;
-    const [cartProducts, setCartProducts] = useState([]);
-    useEffect(() => {
-        if(cartProducts?.length > 0) {
-            ls?.setItem("cart", JSON.stringify(cartProducts));
-        }
-    }, [cartProducts]);
-    useEffect(() => {
-        if (ls && ls.getItem("cart")) {
-            setCartProducts(JSON.parse(ls.getItem("cart")));
-        }
-    }, []);
-    function addProduct(productId) {
-        setCartProducts(prev => [...prev, productId]);
+export function CartContextProvider({ children }) {
+  const ls = typeof window !== "undefined" ? window.localStorage : null;
+  const [cartProducts, setCartProducts] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false); // New state variable
+
+  useEffect(() => {
+    if (!isLoaded && ls && ls.getItem("cart")) {
+      setCartProducts(JSON.parse(ls.getItem("cart")));
+      setIsLoaded(true);
     }
-    function removeProduct(productId) {
-        setCartProducts(prev => {
-            const pos = prev.indexOf(productId);
-            if(pos !== -1) {
-               return prev.filter((value, index) => index !== pos);
-            }
-            return prev;
-        });
+  }, [ls, isLoaded]);
+
+  useEffect(() => {
+    if (isLoaded) {
+      if (cartProducts?.length > 0) {
+        ls?.setItem("cart", JSON.stringify(cartProducts));
+      } else {
+        ls?.removeItem("cart");
+      }
     }
-    return (
-        <CartContext.Provider value={{cartProducts, setCartProducts, addProduct, removeProduct}}>
-            {children}
-        </CartContext.Provider>
-    )
+  }, [cartProducts, ls, isLoaded]);
+
+  function addProduct(productId) {
+    setCartProducts((prev) => [...prev, productId]);
+  }
+
+  function removeProduct(productId) {
+    setCartProducts((prev) => {
+      const pos = prev.indexOf(productId);
+      if (pos !== -1) {
+        return prev.filter((value, index) => index !== pos);
+      }
+      return prev;
+    });
+  }
+
+  function clearCart() {
+    setCartProducts([]);
+  }
+
+  const cartContextValue = {
+    cartProducts,
+    addProduct,
+    removeProduct,
+    clearCart,
+  };
+
+  return (
+    <CartContext.Provider value={cartContextValue}>
+      {children}
+    </CartContext.Provider>
+  );
 }
--- pages/cart.js
+++ pages/cart.js
@@ -51,7 +51,7 @@ const CityHolder = styled.div`
 `;
 
 export default function CartPage() {
-  const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
+  const { cartProducts, addProduct, removeProduct, clearCart } = useContext(CartContext);
   const [products, setProducts] = useState([]);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -60,6 +60,7 @@ export default function CartPage() {
   const [address, setAddress] = useState("");
   const [house, setHouse] = useState("");
   const [complement, setComplement] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
 
   useEffect(() => {
     if (cartProducts.length > 0) {
@@ -70,6 +71,16 @@ export default function CartPage() {
       setProducts([]);
     }
   }, [cartProducts]);
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+    if (window?.location.href.includes("success")) {
+      setIsSuccess(true);
+      clearCart();
+    }
+  }, []);
 
   function plusProduct(id) {
     addProduct(id);
@@ -95,7 +106,7 @@ export default function CartPage() {
     total += price;
   }
 
-  if (window.location.href.includes("success")) {
+  if (isSuccess) {
     return (
       <>
         <Header></Header>
