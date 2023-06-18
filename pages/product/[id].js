df389e2c887628219ea5d891adea7f3a8ce21beb
Guilherme <devguicordeiro@gmail.com>
2023-06-17 22:17:40 -0300

    slow day, tmrw ill give my best

---
+++ pages/product/[id].js
@@ -0,0 +1,14 @@
+import Center from "@/components/center";
+import Header from "@/components/header";
+
+export default function ProductPage() {
+    return (
+        <>
+            <Header>
+                <Center>
+                    
+                </Center>
+            </Header>
+        </>
+    )
+}
