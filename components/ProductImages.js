acd44e122c706458c278b2b3a6bd9b1e47906e3b
Guilherme <devguicordeiro@gmail.com>
2023-06-19 19:20:19 -0300

    added transitional button on images of single product page

--- components/ProductImages.js
+++ components/ProductImages.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { styled } from "styled-components";
 
 const Image = styled.img`
@@ -6,6 +7,15 @@ max-height: 100%;
 border-radius: 5px;
 `;
 
+const MainImage = styled.img`
+max-width: 100%;
+max-height: 200px;
+border-radius: 5px;
+`;
+
+const MainImageWrapper = styled.div`
+    text-align: center;
+`;
 const ImageButtons = styled.div `
 display: flex;
 flex-grow: 0;
@@ -14,6 +24,7 @@ margin-top: 5px;
 `;
 const ImageButton = styled.div `
 border: 2px solid #aaa;
+${props => props.active ? `border-color: fuchsia;` : ` opacity: .9;`}
 border-radius: 5px;
 height: 40px;
 padding: 2px;
@@ -23,13 +34,18 @@ box-sizing: border-box;
 
 export default function ProductImages({images}) {
     
-
+    const [activeImage, setActiveImage] = useState(images?.[0]);
     return(
         <>
-            <Image src={images?.[0]} />
+            <MainImageWrapper>
+                <MainImage src={activeImage} />
+            </MainImageWrapper>
             <ImageButtons>
                 {images.map(image => (
-                    <ImageButton>
+                    <ImageButton
+                    key={image}
+                    active={image === activeImage} 
+                    onClick={() => setActiveImage(image)}>
                         <Image src={image} />
                     </ImageButton>
                 ))}
