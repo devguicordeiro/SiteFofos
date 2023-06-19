2b80af6bfd2140f45190f9a78b66231573944ab2
Guilherme <devguicordeiro@gmail.com>
2023-06-18 21:44:40 -0300

    added route to product's single page

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,14 +1,32 @@
 import Center from "@/components/center";
 import Header from "@/components/header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import { styled } from "styled-components";
 
-export default function ProductPage() {
+const Title = styled.h1`
+    font-size: 1.5em;
+`;
+
+export default function ProductPage({product}) {
     return (
         <>
             <Header>
                 <Center>
-                    
+                    <Title></Title>
                 </Center>
             </Header>
         </>
     )
+}
+
+export async function getServerSideProps(context) {
+    await mongooseConnect();
+    const {id} = context.query;
+    const product = await Product.findById(id);
+    return {
+        props:{
+            product: JSON.parse(JSON.stringify(product)),
+        }
+    }
 }
