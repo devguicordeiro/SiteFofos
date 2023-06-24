e7c0481b8c1e130a283d462758312ae582243f33
Guilherme <devguicordeiro@gmail.com>
2023-06-24 20:45:14 -0300

    start coding categories page

---
+++ pages/categories.js
@@ -0,0 +1,34 @@
+import Center from "@/components/center";
+import Header from "@/components/header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Category } from "@/models/Category";
+import { styled } from "styled-components";
+
+const Title = styled.h1`
+    font-size: 1.5em;
+`;
+
+export default function CategoriesPage({ categories }) {
+    return (
+        <>
+            <Header />
+            <Center>
+                {categories.map(cat => (
+                    <div key={cat.id}>
+                        <h2>{cat.name}</h2>
+                    </div>
+                ))}
+            </Center>
+        </>
+    );
+}
+
+export async function getServerSideProps() {
+    await mongooseConnect();
+    const categories = await Category.find({ parent: null }).maxTimeMS(30000);
+    return {
+        props: {
+            categories: JSON.parse(JSON.stringify(categories)),
+        },
+    };
+}
