0a63289563a8e92c0c368f78825e070d80168e30
Guilherme <devguicordeiro@gmail.com>
2023-06-24 23:18:49 -0300

    after solving lots of acumulated warning, ready to proceed

--- pages/categories.js
+++ pages/categories.js
@@ -2,18 +2,19 @@ import Center from "@/components/center";
 import Header from "@/components/header";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
+import { Product } from "@/models/Product";
 import { styled } from "styled-components";
 
 const Title = styled.h1`
     font-size: 1.5em;
 `;
 
-export default function CategoriesPage({ categories }) {
+export default function CategoriesPage({ mainCategories, categoriesProducts }) {
     return (
         <>
             <Header />
             <Center>
-                {categories.map(cat => (
+                {mainCategories.map(cat => (
                     <div key={cat._id}>
                         <h2>{cat.name}</h2>
                     </div>
@@ -25,10 +26,17 @@ export default function CategoriesPage({ categories }) {
 
 export async function getServerSideProps() {
     await mongooseConnect();
-    const categories = await Category.find({ parent: null }).maxTimeMS(30000);
+    const categories = await Category.find().maxTimeMS(30000);
+    const mainCategories = categories.filter(c => !c.parent);
+    const categoriesProducts = {}; 
+    for (const mainCat of mainCategories) {
+        const products = await Product.find({category: mainCat._id}, null, {limit:3, sort:{"_id": -1}});
+        categoriesProducts[mainCat._id] = products;
+    }
     return {
         props: {
-            categories: JSON.parse(JSON.stringify(categories)),
+            mainCategories: JSON.parse(JSON.stringify(mainCategories)),
+            categoriesProducts: JSON.parse(JSON.stringify(categoriesProducts)),
         },
     };
 }
