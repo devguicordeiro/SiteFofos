dab9e9f28bf641457a27af802ec9e902a9ab2c91
Guilherme <devguicordeiro@gmail.com>
2023-06-26 11:18:40 -0300

    select dropdown working

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -3,6 +3,7 @@ import Center from "@/components/center";
 import Header from "@/components/header";
 import { Category } from "@/models/Category";
 import { Product } from "@/models/Product";
+import { useState } from "react";
 import { styled } from "styled-components";
 
 const Title = styled.h1`
@@ -36,6 +37,18 @@ const Filter = styled.div `
 `;
 
 export default function CategoryPage({category, products}) {
+    const [filtersValues, setFiltersValues] = useState(category.properties
+        .map(p => ({name:p.name, value:"all"})));
+    function handleFilterChange(filterName, filterValue) {
+        setFiltersValues(prev => {
+            const newValues = prev.map(p => ({
+                name: p.name,
+                value: p.name === filterName ? filterValue : p.value,
+            }));
+            return newValues;
+        });
+    }
+
     return (
         <>
             <Header></Header>
@@ -44,11 +57,14 @@ export default function CategoryPage({category, products}) {
                     <Title>{category.name}</Title>
                     <FiltersWrapper>
                         {category.properties.map(prop => (
-                            <Filter>
-                                <span>{prop.name}</span>
-                                <select>
+                            <Filter key={prop.name}>
+                                <span>{prop.name}:</span>
+                                <select
+                                onChange={ev => handleFilterChange(prop.name, ev.target.value)} 
+                                value={filtersValues.find(f => f.name === prop.name).value}>
+                                    <option value="all">Nenhuma</option>
                                     {prop.values.map(val => (
-                                        <option value={val}>
+                                        <option key={val} value={val}>
                                             {val}
                                         </option>
                                     ))}
