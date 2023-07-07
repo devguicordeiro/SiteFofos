380d9681b1bf533b7a1e50fdfcad51a900f8e6c4
Guilherme <devguicordeiro@gmail.com>
2023-07-07 20:20:29 -0300

    added model for wishedp and improvements in index

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import NewProducts from "@/components/NewProducts";
 import Header from "@/components/header";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { WishedProduct } from "@/models/WishedProduct"
 
 export default function HomePage({featuredProduct, newProducts}) {
   return (
@@ -14,11 +17,17 @@ export default function HomePage({featuredProduct, newProducts}) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(ctx) {
   const featuredProductId = "649cb35f5a19a26d55334838";
   await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
   const newProducts = await Product.find({}, null, {sort: {"_id":-1}, limit:12});
+  const {user} = await getServerSession(ctx.req, ctx.res, authOptions)
+  const wishedNewProducts = await WishedProduct?.find({
+    userEmail:user.email,
+    product: newProducts.map(p => p._id.toString()),
+  });
+  console.log(wishedNewProducts);
   return {
     props: {featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
             newProducts: JSON.parse(JSON.stringify(newProducts)),
