aef58932bc0676e90bd91519ff83b7953124aef8
Guilherme <devguicordeiro@gmail.com>
2023-07-08 20:18:15 -0300

    working wished btn

--- pages/api/wishlist.js
+++ pages/api/wishlist.js
@@ -1,9 +1,20 @@
 import { mongooseConnect } from "@/lib/mongoose";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+import { WishedProduct } from "@/models/WishedProduct";
 
 export default async function handle(req, res) {
     await mongooseConnect();
+    const {user} = await getServerSession(req, res, authOptions);
     if (req.method === "POST") {
-        const {productId} = req.body;
-        res.json(productId);
+        const {product} = req.body;
+        const wishedDoc = await WishedProduct.findOne({userEmail:user.email,product});
+        if  (wishedDoc) {
+            await WishedProduct.findByIdAndDelete(wishedDoc._id);
+            res.json("deleted");
+        } else {
+            await WishedProduct.create({userEmail:user.email,product});
+            res.json("created");
+        }
     }
 }
