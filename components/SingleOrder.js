06eef0b30886b94c0f9013f611b836ab7fa05a7c
Guilherme <devguicordeiro@gmail.com>
2023-07-10 22:14:54 -0300

    account styling almost complete. gonna rest

--- components/SingleOrder.js
+++ components/SingleOrder.js
@@ -1,12 +1,42 @@
+import { styled } from "styled-components"
+
+const StyledOrder = styled.div`
+    background-color: fuchsia;
+    margin: 10px 0;
+    padding: 10px;
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    align-items: center;
+    gap: 30px;
+    time{
+        font-size: 0.9rem;
+        font-weight: bold;
+        color: purple
+    }
+`;
+const ProductRow = styled.div`
+    span{
+        border: 1px solid black;
+        color: purple;
+        padding: 0 2px;
+        border-radius: 5px;
+    }
+`;
+
 export default function SingleOrder({line_items, createdAt}) {
     return(
-        <div>
-            <time>{createdAt}</time>
-            {line_items.map(item => (
-                <div>
-                    {item.price_data.product_data.name}
-                </div>
-            ))}
-        </div>
+        <StyledOrder>
+            <div>
+                <time>{(new Date(createdAt)).toLocaleString("pt-BR")}</time>
+            </div>
+            <div>
+                {line_items.map(item => (
+                    <ProductRow>
+                        <span>{item.quantity} x</span> {item.price_data.product_data.name}
+                    </ProductRow>
+                ))}
+            </div>
+
+        </StyledOrder>
     )
 }
